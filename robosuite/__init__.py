6973c6816ac7d9de5c51aebac80a39309403c22f
wangzizhao <zzwang@umich.edu>
2021-07-11 19:14:01 -0500

    add causal environment

--- robosuite/__init__.py
+++ robosuite/__init__.py
@@ -11,6 +11,8 @@ from robosuite.environments.manipulation.two_arm_lift import TwoArmLift
 from robosuite.environments.manipulation.two_arm_peg_in_hole import TwoArmPegInHole
 from robosuite.environments.manipulation.two_arm_handover import TwoArmHandover
 
+from robosuite.environments.manipulation.causal1 import Causal1
+
 from robosuite.environments import ALL_ENVIRONMENTS
 from robosuite.controllers import ALL_CONTROLLERS, load_controller_config
 from robosuite.robots import ALL_ROBOTS
