860eba8427dcfc3184fc20a6536d789a45b22e84
wangzizhao <zzwang@umich.edu>
2022-08-17 00:29:04 -0500

    clean tool and pot setup

--- robosuite/models/tool_use/lshape_tool.py
+++ robosuite/models/tool_use/lshape_tool.py
@@ -1,7 +1,6 @@
 import numpy as np
-from robosuite.models.objects import MujocoXMLObject, CompositeObject
-from robosuite.utils.mjcf_utils import xml_path_completion, array_to_string, find_elements, CustomMaterial, add_to_dict, RED, GREEN, BLUE
-from robosuite.models.objects import BoxObject
+from robosuite.models.objects import CompositeObject
+from robosuite.utils.mjcf_utils import CustomMaterial, add_to_dict
 import robosuite.utils.transform_utils as T
 
 
@@ -9,19 +8,15 @@ class LShapeTool(CompositeObject):
     def __init__(
             self,
             name,
-            tunnel_size=(0.1, 0.015, 0.06),
             density=100,
             use_texture=True):
 
         self._name = name
 
-        self.length = 0
-        self.height = 0
-
         self.use_texture = use_texture
 
         base_args = {
-            "total_size": self.length / 2.0,
+            "total_size": 0 / 2.0,
             "name": self.name,
             "locations_relative_to_center": True,
             "obj_types": "all",
@@ -29,72 +24,36 @@ class LShapeTool(CompositeObject):
         site_attrs = []
         obj_args = {}
 
-        # Long sides
-        flat_size = [tunnel_size[0] - 0.04, tunnel_size[1], 0.005]
-        side_size = [0.02, tunnel_size[1], tunnel_size[2] + 0.005]
-
-        r = np.pi / 2
-
         geom_mat = "MatRedWood"
-        pot_width = 0.0
-        pot_length = 0.06
-
-        edge_width = 0.007
-        geom_frictions = (0.005, 0.005, 0.0001)
+        geom_frictions = (1, 0.005, 0.0001)
 
         solref = (0.02, 1.)
         
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(0., 0., 0.0),
+            geom_locations=(0.05, 0.0, 0.0),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([0.11, 0.011, 0.02]),
+            geom_sizes=np.array([0.11, 0.01, 0.02]),
             geom_names=f"body_0",
             geom_rgbas=None,
             geom_materials=geom_mat,
-            geom_frictions=(0.0005, 0.005, 0.0001),
-            # geom_frictions=(0.0, 0.0, 0.0),
+            geom_frictions=geom_frictions,
             solref=solref,
-        )
+            density=density)
 
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(0.11, 0.05, 0.0),
+            geom_locations=(0.16, 0.05, 0.0),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([0.006, 0.06, 0.02]),
+            geom_sizes=np.array([0.01, 0.06, 0.02]),
             geom_names=f"body_1",
             geom_rgbas=None,
             geom_materials=geom_mat,
             geom_frictions=geom_frictions,
             solref=solref,
-            density=density * 0.1)
-
-        bottom_site = self.get_site_attrib_template()
-        top_site = self.get_site_attrib_template()
-        horizontal_site = self.get_site_attrib_template()
-
-        bottom_site.update({
-            "name": "bottom",
-            "pos": array_to_string(np.array([0., 0., -2 * self.height])),
-            "size": "0.005",
-            "rgba": "0 0 0 0"
-        })
-
-        top_site.update({
-            "name": "top",
-            "pos": array_to_string(np.array([0., 0., 2 * self.height])),
-            "size": "0.005",
-            "rgba": "0 0 0 0"
-        })
-
-        bottom_site.update({
-            "name": "bottom",
-            "pos": array_to_string(np.array([0., 0., -2 * self.height])),
-            "size": "0.005",
-            "rgba": "0 0 0 0"
-        })
+            density=density)
 
         obj_args.update(base_args)
 
@@ -102,7 +61,6 @@ class LShapeTool(CompositeObject):
         obj_args["joints"] = [{"type": "free", "damping":"0.0005"}]
 
         super().__init__(**obj_args)
-        
 
         tex_attrib = {
             "type": "cube",
@@ -112,24 +70,24 @@ class LShapeTool(CompositeObject):
             "specular": "0.4",
             "shininess": "0.1",
         }
-        steel_scratched_material = CustomMaterial(
+        redwood = CustomMaterial(
             texture="WoodRed",
             tex_name="red_wood",
             mat_name="MatRedWood",
             tex_attrib=tex_attrib,
             mat_attrib=mat_attrib,
         )
- 
-        self.append_material(steel_scratched_material)
+
+        self.append_material(redwood)
 
     @property
     def bottom_offset(self):
-        return np.array([0, 0, -2 * self.height])
+        return np.array([0, 0, -0.02])
 
     @property
     def top_offset(self):
-        return np.array([0, 0, 2 * self.height])
-        
+        return np.array([0, 0, 0.02])
+
     @property
     def horizontal_radius(self):
-        return self.length * np.sqrt(2)
+        return 0  # np.sqrt(0.11 ** 2 + 0.17 ** 2)
--- robosuite/models/tool_use/pot_object.py
+++ robosuite/models/tool_use/pot_object.py
@@ -1,27 +1,35 @@
 import numpy as np
-from robosuite.models.objects import MujocoXMLObject, CompositeObject
-from robosuite.utils.mjcf_utils import xml_path_completion, array_to_string, find_elements, CustomMaterial, add_to_dict, RED, GREEN, BLUE
-from robosuite.models.objects import BoxObject
+from robosuite.models.objects import CompositeObject
+from robosuite.utils.mjcf_utils import array_to_string, CustomMaterial, add_to_dict
 import robosuite.utils.transform_utils as T
-
 
 
 class PotObject(CompositeObject):
     def __init__(
             self,
             name,
-            tunnel_size=(0.1, 0.015, 0.06),
-            density=1000,
+            pot_size=(0.06, 0.06, 0.03),
+            bottom_half_thickness=0.005,
+            side_half_thickness=0.0075,
+            handle_half_height=0.01,
+            handle_half_length=0.04,
+            density=100,
             use_texture=True):
 
         self._name = name
-        self.length = tunnel_size[1]
-        self.height = tunnel_size[2]
+
+        pot_half_x_length, pot_half_y_length, pot_half_height = pot_size
+        self.pot_half_x_length = pot_half_x_length
+        self.pot_half_y_length = pot_half_y_length
+        self.pot_half_height = pot_half_height
+        self.bottom_half_thickness = bottom_half_thickness
+        self.side_half_thickness = side_half_thickness
+        self.handle_half_height = handle_half_height
 
         self.use_texture = use_texture
 
         base_args = {
-            "total_size": self.length / 2.0,
+            "total_size": 0.0,
             "name": self.name,
             "locations_relative_to_center": True,
             "obj_types": "all",
@@ -29,42 +37,32 @@ class PotObject(CompositeObject):
         site_attrs = []
         obj_args = {}
 
-        # Long sides
-        flat_size = [tunnel_size[0] - 0.04, tunnel_size[1], 0.005]
-        side_size = [0.02, tunnel_size[1], tunnel_size[2] + 0.005]
-
-        r = np.pi / 2
-
         geom_mat = "steel_scratched_mat"
-        pot_width = 0.0
-        pot_length = 0.06
-
-        edge_width = 0.007
-        geom_frictions = (0.005, 0.005, 0.0001)
-
+        geom_frictions = (1, 0.005, 0.0001)
         solref = (0.02, 1.)
-        
+
+        # bottom
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(0., 0., 0.0025),
+            geom_locations=(0., 0., bottom_half_thickness),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([pot_length - 0.01 - 0.007, pot_length - 0.007, 0.005]),
+            geom_sizes=np.array([pot_half_x_length - side_half_thickness,
+                                 pot_half_y_length - side_half_thickness,
+                                 bottom_half_thickness]),
             geom_names=f"body_0",
             geom_rgbas=None,
             geom_materials=geom_mat,
-            geom_frictions=(0.0005, 0.005, 0.0001),
-            # geom_frictions=(0.0, 0.0, 0.0),
+            geom_frictions=geom_frictions,
             solref=solref,
         )
 
-        pot_height = 0.035
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(pot_width, -pot_length, pot_height - 0.0025),
+            geom_locations=(0, -pot_half_y_length, pot_half_height),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([pot_length + 0.007 - 0.01, edge_width, pot_height]),
+            geom_sizes=np.array([pot_half_x_length + side_half_thickness, side_half_thickness, pot_half_height]),
             geom_names=f"body_1",
             geom_rgbas=None,
             geom_materials=geom_mat,
@@ -75,9 +73,9 @@ class PotObject(CompositeObject):
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(pot_width, pot_length, pot_height - 0.0025),
+            geom_locations=(0, pot_half_y_length, pot_half_height),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([pot_length + 0.007 - 0.01, edge_width, pot_height]),
+            geom_sizes=np.array([pot_half_x_length + side_half_thickness, side_half_thickness, pot_half_height]),
             geom_names=f"body_2",
             geom_rgbas=None,
             geom_materials=geom_mat,
@@ -88,9 +86,9 @@ class PotObject(CompositeObject):
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(pot_length - 0.01, pot_width, pot_height - 0.0025),
+            geom_locations=(pot_half_x_length, 0, pot_half_height),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([0.007, pot_length - 0.007, pot_height]),
+            geom_sizes=np.array([side_half_thickness, pot_half_y_length + side_half_thickness, pot_half_height]),
             geom_names=f"body_3",
             geom_rgbas=None,
             geom_materials=geom_mat,
@@ -101,9 +99,9 @@ class PotObject(CompositeObject):
         add_to_dict(
             dic=obj_args,
             geom_types="box",
-            geom_locations=(-pot_length + 0.01, pot_width, pot_height - 0.0025),
+            geom_locations=(-pot_half_x_length, 0, pot_half_height),
             geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-            geom_sizes=np.array([0.007, pot_length - 0.007, pot_height]),
+            geom_sizes=np.array([side_half_thickness, pot_half_y_length + side_half_thickness, pot_half_height]),
             geom_names=f"body_4",
             geom_rgbas=None,
             geom_materials=geom_mat,
@@ -111,58 +109,28 @@ class PotObject(CompositeObject):
             solref=solref,                                    
             density=density)
 
-        handle_radius = 0.01
-        handle_width = 0.055
-        handle_length = edge_width * 2
         handle_friction = 1.0
 
-        for (direction, y) in zip(['left', 'right'], [pot_length , -pot_length]):
+        for (direction, y) in zip(['left', 'right'], [pot_half_y_length, -pot_half_y_length]):
             add_to_dict(
                 dic=obj_args,
                 geom_types="box",
-                geom_locations=(0.0, y, 2 * pot_height + 0.007),
+                geom_locations=(0.0, y, 2 * pot_half_height + handle_half_height),
                 geom_quats=T.convert_quat(T.axisangle2quat(np.array([0, 0, 0])), to="wxyz"),
-                geom_sizes=np.array([handle_width / 2, handle_length / 2, handle_radius]),
+                geom_sizes=np.array([handle_half_length, side_half_thickness, handle_half_height]),
                 geom_names=f"handle_{direction}_1",
                 geom_rgbas=None,
                 geom_materials=geom_mat,
-                # geom_frictions=(handle_friction, 0.005, 0.0001),
-                geom_frictions=geom_frictions,
+                geom_frictions=(handle_friction, 0.005, 0.0001),
                 solref=solref,                                
                 density=density)
-        
-        bottom_site = self.get_site_attrib_template()
-        top_site = self.get_site_attrib_template()
-        horizontal_site = self.get_site_attrib_template()
-
-        bottom_site.update({
-            "name": "bottom",
-            "pos": array_to_string(np.array([0., 0., -2 * self.height])),
-            "size": "0.005",
-            "rgba": "0 0 0 0"
-        })
-
-        top_site.update({
-            "name": "top",
-            "pos": array_to_string(np.array([0., 0., 2 * self.height])),
-            "size": "0.005",
-            "rgba": "0 0 0 0"
-        })
-
-        bottom_site.update({
-            "name": "bottom",
-            "pos": array_to_string(np.array([0., 0., -2 * self.height])),
-            "size": "0.005",
-            "rgba": "0 0 0 0"
-        })
 
         obj_args.update(base_args)
 
         obj_args["sites"] = site_attrs
-        obj_args["joints"] = [{"type": "free", "damping":"0.0005"}]
+        obj_args["joints"] = [{"type": "free", "damping": "0.0005"}]
 
         super().__init__(**obj_args)
-        
 
         tex_attrib = {
             "type": "cube",
@@ -184,12 +152,13 @@ class PotObject(CompositeObject):
 
     @property
     def bottom_offset(self):
-        return np.array([0, 0, -2 * self.height])
+        return np.array([0, 0, -self.bottom_half_thickness])
 
     @property
     def top_offset(self):
-        return np.array([0, 0, 2 * self.height])
-        
+        return np.array([0, 0, self.bottom_half_thickness + 2 * self.pot_half_height + 2 * self.handle_half_height])
+
     @property
     def horizontal_radius(self):
-        return self.length * np.sqrt(2)
+        return np.linalg.norm([self.pot_half_x_length + self.side_half_thickness,
+                               self.pot_half_y_length + self.side_half_thickness])
